445bb47b9c0e4c6ddefa2d8d99ce61c308e7d1a5
Amanda Pearce <amandagpearce@gmail.com>
2023-06-08 18:33:12 -0300

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './app.css';
 
 const App = () => {
-  return <h1>Yay</h1>;
+  return (
+    <main className="bg-black">
+      <h1 className="text-secondary text-4xl font-bold">Yay</h1>
+    </main>
+  );
 };
 
 export default App;
