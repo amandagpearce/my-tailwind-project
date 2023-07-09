ef06040ec328cf86480de78fea317604956a7928
Amanda Pearce <amandagpearce@gmail.com>
2023-07-09 18:24:18 -0300

    removes google script from index.html and adds it to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,27 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const rootNode = document.getElementById('root');
+// Get the Google API key from the environment variable
+const googleApiKey = process.env.REACT_GOOGLE_KEY;
+
+// Function to load the Google Maps script dynamically
+const loadGoogleMapsScript = () => {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${googleApiKey}&libraries=places`;
+    script.async = true;
+    script.onload = resolve;
+    script.onerror = reject;
+    document.body.appendChild(script);
+  });
+};
 
 if (rootNode) {
-  createRoot(rootNode).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  loadGoogleMapsScript().then(() => {
+    createRoot(rootNode).render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+  });
 }
